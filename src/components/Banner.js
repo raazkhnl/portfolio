b2afe3538a8f378b090b145eee9c46156f450308
raazkhnl <raazkhnl@gmail.com>
2025-04-08 14:29:21 +0545

    Refactor App component to include Router and update Banner component for improved text rotation and linking

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,7 @@ import headerImg from "../assets/img/header-img.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { HashLink } from "react-router-hash-link";
 
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -11,45 +12,39 @@ const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Computer Engineer","Web Developer", "Programmer" , "UI/UX Designer" ];
+  const toRotate = ["Computer Engineer", "Frontend Developer", "Backend Developer", "Python Programmer", "MERN Stack Developer", "Data Analyst"];
   const period = 2000;
 
- 
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(index)
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
   useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
+    const tick = () => {
+      const i = loopNum % toRotate.length;
+      const fullText = toRotate[i];
+      const updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
 
-    return () => { clearInterval(ticker) };
-    // eslint-disable-next-line
-  }, [text])
+      setText(updatedText);
+
+      if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+      }
+
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(prev => prev + 1);
+        setIndex(1);
+        setDelta(500);
+      } else {
+        setIndex(prev => prev + 1);
+      }
+    };
+
+    const ticker = setInterval(tick, delta);
+    return () => clearInterval(ticker);
+  }, [text, delta, isDeleting, loopNum]);
 
   return (
     <section className="banner" id="home">
@@ -57,27 +52,31 @@ const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-              <h1>{`Hi! I'm RaaZ `} <span className="txt-rotate" dataperiod="1000" data-rotate={toRotate}><span className="wrap">{text}</span></span></h1>
+              {({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>{`Hi! I'm RaaZ, `}<span className="txt-rotate"><span className="wrap">{text}</span></span></h1>
                   <p>Welcome to my digital realm! I'm RaaZ Khanal, an innovative computer engineer crafting elegant code and designing seamless experiences. Join me on a limitless journey where technology and imagination converge. Together, let's turn ideas into reality and shape the digital landscape.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+                  <HashLink to="#connect" style={{ textDecoration: 'none' }}>
+                    <button>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  </HashLink>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
+              {({ isVisible }) => (
                 <div className={isVisible ? "animate__animated animate__slideInRight" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
+                  <img src={headerImg} alt="Header" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,65 +1,54 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-
 import { BsFacebook, BsInstagram, BsGithub } from "react-icons/bs";
 
 const NavBar = () => {
-
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }
+      setScrolled(window.scrollY > 50);
+    };
 
     window.addEventListener("scroll", onScroll);
-
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
-    <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="/">
-            <p style={{color:'white', fontWeight: 'bold'}}>RaaZ Khanal</p>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a target="_blank" rel="noreferrer" href="https://facebook.com/raazkhnl"  ><span><BsFacebook color="white"/></span></a>
-                <a target="_blank" rel="noreferrer" href="https://instagram.com/raazkhnl"><span><BsInstagram color="white"/></span></a>
-                <a target="_blank" rel="noreferrer" href="https://github.com/raazkhnl"><span><BsGithub color="white"/></span></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Router>
-  )
-}
-export default NavBar
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/">
+          <p style={{ color: 'white', fontWeight: 'bold' }}>RaaZ Khanal</p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a target="_blank" rel="noreferrer" href="https://facebook.com/raazkhnl"><BsFacebook color="white" /></a>
+              <a target="_blank" rel="noreferrer" href="https://instagram.com/raazkhnl"><BsInstagram color="white" /></a>
+              <a target="_blank" rel="noreferrer" href="https://github.com/raazkhnl"><BsGithub color="white" /></a>
+            </div>
+            <HashLink to="#connect">
+              <button className="vvd"><span>Let’s Connect</span></button>
+            </HashLink>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
